dbc1dd354bd92eb6789ff892c128ddcfa17b62d4
djoma98 <djoma98@outlook.com>
2023-09-01 00:19:35 +0200

    font fixes and other stuff

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "monospace:size=16", "Font Awesome 5 Brands:size=16", "Font Awesome 5 Free Solid:size=16"};
+static const char dmenufont[]       = "monospace:size=16";
 //background color
 static const char col_gray1[]       = "#222222";
 //inactive window border color
@@ -108,10 +108,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,              		    XK_n,      shiftview,  	   { .i = +1 } },
 	{ MODKEY,              		    XK_b,      shiftview,      { .i = -1 } },
-    { MODKEY,                       XK_F8,     spawn,          {.v = upvol   } },
-    { MODKEY,                       XK_F7,     spawn,          {.v = downvol } },
-    { MODKEY,                       XK_F5,     spawn,          {.v = mutevol } },
-	TAGKEYS(                        XK_1,                      0)
+	{ 0,                            0x1008ff02, spawn,         SHCMD ("xbacklight -inc 10")},
+	{ 0,                            0x1008ff03, spawn,         SHCMD ("xbacklight -dec 10")},
+	{ 0,                            0x1008ff11, spawn,         SHCMD ("amixer sset Master 5%- unmute")},
+	{ 0,                            0x1008ff12, spawn,         SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")},
+	{ 0,                            0x1008ff13, spawn,         SHCMD ("amixer sset Master 5%+ unmute")},
+      	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
